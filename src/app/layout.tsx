894de41e67bfddc8efe10eb354c92ba80d98ee0b
seseseee <135571934+seseseee@users.noreply.github.com>
2025-12-12 19:56:55 +0900

    Delete src/app directory

--- src/app/layout.tsx
+++
@@ -1,9 +0,0 @@
-export const metadata = { title: "ChatViz Dashboard" };
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="ja">
-      <body>{children}</body>
-    </html>
-  );
-}
